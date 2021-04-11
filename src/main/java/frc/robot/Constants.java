bbe4522eaee3b2f1ce0178f842ec360ea46e99f4
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2021-04-11 13:29:37 -0700

    Clean up constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,49 +7,39 @@ import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import frc.robot.vision.*;
+import frc.robot.units.*;
 
 
 public final class Constants {
+    //TODO replace -1 ports with actual port numbers
     public static class Ports {
         //Laptop ports
         public static int PORT_JOYSTICK_DRIVE = 0;
         public static int PORT_JOYSTICK_OPERATOR = 1;
 
         //RoboRIO sensor ports
-        public static int DIO_BREAKBEAM_INTAKE = 4;
-        public static int DIO_BREAKBEAM_HOPPER = 5;
-        public static int DIO_BREAKBEAM_STORAGE_LOW = 6;
-        public static int DIO_BREAKBEAM_STORAGE_HIGH = 7;
-        public static int DIO_LIMIT_CLIMB_TOP_1 = 2;
-        public static int DIO_LIMIT_CLIMB_TOP_2 = 3;
-        public static int DIO_LIMIT_CLIMB_BOTTOM_1 = 0;
-        public static int DIO_LIMIT_CLIMB_BOTTOM_2 = 1;
         public static SPI.Port SPI_PORT_GYRO = SPI.Port.kMXP;
         //Below is format for analog sensors
         //public static int PWM_NAME = -1;
 
         //Solenoid PCM ports
-        public static int PCM_CLIMB = 7;
-        public static int PCM_INTAKE_FORWARD = 1;
-        public static int PCM_INTAKE_REVERSE = 2;
+        public static int PCM_INTAKE_FORWARD = -1;
+        public static int PCM_INTAKE_REVERSE = -1;
+        public static int PCM_SHOOTER_FORWARD = -1;
+        public static int PCM_SHOOTER_REVERSE = -1;
 
         //CAN Bus IDs
         public static int CAN_PDP = 0;
         public static int CAN_PCM = 0;
-        public static int CAN_DRIVETRAIN_RIGHT_TALONSRX = 0;
-        public static int CAN_DRIVETRAIN_RIGHT_VICTORSPX = 1;
-        public static int CAN_DRIVETRAIN_LEFT_TALONSRX = 15;
-        public static int CAN_DRIVETRAIN_LEFT_VICTORSPX = 14;
-        public static int CAN_SHOOTER_TALONSRX = 2;
-        public static int CAN_SHOOTER_SLAVE_TALONSRX = 3;
-        public static int CAN_SHOOTER_INSERTER_TALONSRX = 5;
-        public static int CAN_CLIMB_MAIN_TALONSRX = 13;
-        public static int CAN_CLIMB_BALANCE_TALONSRX = 6;
-        public static int CAN_STORAGE_TALONSRX = 7;
-        public static int CAN_INTAKE_TALONSRX = 8;
-        public static int CAN_SPINNER_TALONSRX = -1;
-        public static int CAN_LEFT_HOPPER_TALONSRX = 21;
-        public static int CAN_RIGHT_HOPPER_TALONSRX = 20;
+        public static int CAN_DRIVETRAIN_RIGHT_TALONSRX = -1;
+        public static int CAN_DRIVETRAIN_RIGHT_VICTORSPX = -1;
+        public static int CAN_DRIVETRAIN_LEFT_TALONSRX = -1;
+        public static int CAN_DRIVETRAIN_LEFT_VICTORSPX = -1;
+        public static int CAN_SHOOTER_TALONSRX = -1;
+        public static int CAN_SHOOTER_SLAVE_TALONSRX = -1;
+        public static int CAN_SHOOTER_INSERTER_TALONSRX = -1;
+        public static int CAN_STORAGE_TALONSRX = -1;
+        public static int CAN_INTAKE_TALONSRX = -1;
     }
 
     public static class MotorConfig {
@@ -62,25 +52,61 @@ public final class Constants {
     }
 
     public static class RobotMeasurements {
-        //Robot physical characteristics
-        public static double DRIVETRAIN_TRACKWIDTH_METERS = 0.606425;
-        public static double DRIVETRAIN_WHEEL_RADIUS_METERS = 0.0762;//3 inches
-        public static double SHOOTER_HEIGHT_METERS = 0;
-        public static double SHOOTER_ANGLE_RADIANS = 0;
-        public static double LIMELIGHT_HEIGHT_METERS = 0;
-        public static double LIMELIGHT_ANGLE_RADIANS = 0;
+        //TODO find robot physical characteristics
+        public static double DRIVETRAIN_TRACKWIDTH = -1;
+        public static double DRIVETRAIN_WHEEL_RADIUS = -1;
+
+        public static double SHOOTER_HEIGHT_METERS = -1;
+        public static double SHOOTER_ANGLE_RADIANS = -1;
+
+        public static double LIMELIGHT_HEIGHT_METERS = -1;
+        public static double LIMELIGHT_ANGLE_RADIANS = -1;
     }
     
-    public static class Conversions {
-        //Conversions
-        public static double ENCODER_VELOCITY_UNIT_TO_SECONDS = 0.1;//Encoder measures things in units per 0.1s
+    public static class Units {
+        //Base units
+        Unit METER = new BaseUnit(Dimension.Length, 1d);
+        Unit KILOMETER = new BaseUnit(Dimension.Length, METER.getScalar() * 1000d);
+        Unit FEET = new BaseUnit(Dimension.Length, METER.getScalar() * 3.280839895d);
+
+        Unit SECOND = new BaseUnit(Dimension.Time, 1d);
+        Unit MINUTE = new BaseUnit(Dimension.Time, SECOND.getScalar() * 60d);
+        Unit HOUR = new BaseUnit(Dimension.Time, MINUTE.getScalar() * 60d);
+        Unit MILLISECOND = new BaseUnit(Dimension.Time, SECOND.getScalar() / 1000d);
+        Unit ENCODER_TIME = new BaseUnit(Dimension.Time, MILLISECOND.getScalar() * 100d);
+        
+        Unit KILOGRAM = new BaseUnit(Dimension.Mass, 1d);
+
+        Unit RADIAN = new BaseUnit(Dimension.Angle, 1d);
+        Unit ROTATION = new BaseUnit(Dimension.Angle, RADIAN.getScalar() * 2d * Math.PI);
+        Unit DEGREE = new BaseUnit(Dimension.Angle, ROTATION.getScalar() / 360d);
+        Unit ENCODER_ANGLE = new BaseUnit(Dimension.Angle, ROTATION.getScalar() / MotorConfig.TALON_ENCODER_RESOLUTION);
+
+        Unit AMPERE = new BaseUnit(Dimension.Current, 1d);
+
+        //Compound units
+        Unit ENCODER_ANGULAR_VELOCITY = new CompoundUnit(ENCODER_ANGLE, ENCODER_TIME);
+
+        Unit METERS_PER_SECOND = new CompoundUnit(METER, SECOND);
+
+        Unit METERS_PER_SECOND_2 = new CompoundUnit(METERS_PER_SECOND, SECOND);
+        Unit NEWTON = new CompoundUnit(new Unit[] {KILOGRAM, METERS_PER_SECOND_2}, new Unit[] {});
+        Unit JOULE = new CompoundUnit(new Unit[] {NEWTON, METER}, new Unit[] {});
+        Unit COULOMB = new CompoundUnit(new Unit[] {AMPERE, SECOND}, new Unit[] {});
+        Unit VOLTAGE = new CompoundUnit(JOULE, COULOMB);
+
+        
+        //TODO How to implement scalar multipliers and angular->velocity?
+        
+        //Old Code
+        //public static double ENCODER_VELOCITY_UNIT_TO_SECONDS = 0.1;//Encoder measures things in units per 0.1s
         public static double DRIVETRAIN_ENCODER_DISTANCE_TO_METERS = 1 / MotorConfig.TALON_ENCODER_RESOLUTION * 2 * Math.PI * RobotMeasurements.DRIVETRAIN_WHEEL_RADIUS_METERS;
         public static double DRIVETRAIN_ENCODER_VELOCITY_TO_METERS_PER_SECOND = DRIVETRAIN_ENCODER_DISTANCE_TO_METERS / ENCODER_VELOCITY_UNIT_TO_SECONDS;
         public static double METERS_PER_SECOND_TO_DRIVETRAIN_ENCODER_VELOCITY = 1 / DRIVETRAIN_ENCODER_VELOCITY_TO_METERS_PER_SECOND;
         public static double SHOOTER_ENCODER_VELOCITY_TO_METERS_PER_SECOND = 0;
         public static double SPINNER_ENCODER_VELOCITY_TO_METERS_PER_SECOND = 0;
-        public static double DRIVETRAIN_FEEDFORWARD_TO_ENCODER_UNITS = 1;//TODO find this number
-        public static double SHOOTER_FEEDFORWARD_TO_ENCODER_UNITS = 1;//TODO find this number
+        //public static double DRIVETRAIN_FEEDFORWARD_TO_ENCODER_UNITS = 1;//TODO find this number
+        //public static double SHOOTER_FEEDFORWARD_TO_ENCODER_UNITS = 1;//TODO find this number
     }
 
     public static class SubsystemConfig {
