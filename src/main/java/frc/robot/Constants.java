162a15e808da3149844954c6d0378824e3b499ae
Dat-Boi-Arjun <56085610+Dat-Boi-Arjun@users.noreply.github.com>
2021-10-28 16:48:47 -0700

    added drive code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,8 +1,8 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.FollowerType;
-import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
+// import com.ctre.phoenix.motorcontrol.FollowerType;
+// import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
+// import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
 
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
@@ -42,14 +42,14 @@ public final class Constants {
         public static int CAN_INTAKE_TALONSRX = -1;
     }
 
-    public static class MotorConfig {
-        //Talon and Victor information
-        public static double TALON_ENCODER_RESOLUTION = 4096; // = EPR = CPR
-        public static int TALON_TIMEOUT_MS = 5;
-        public static int TALON_DEFAULT_PID_ID = 0;//0 is primary, 1 is auxilary
-        public static TalonSRXFeedbackDevice TALON_DEFAULT_FEEDBACK_DEVICE = TalonSRXFeedbackDevice.CTRE_MagEncoder_Relative;
-        public static FollowerType DEFAULT_MOTOR_FOLLOWER_TYPE = FollowerType.PercentOutput;
-    }
+    // public static class MotorConfig {
+    //     //Talon and Victor information
+    //     public static double TALON_ENCODER_RESOLUTION = 4096; // = EPR = CPR
+    //     public static int TALON_TIMEOUT_MS = 5;
+    //     public static int TALON_DEFAULT_PID_ID = 0;//0 is primary, 1 is auxilary
+    //     public static TalonSRXFeedbackDevice TALON_DEFAULT_FEEDBACK_DEVICE = TalonSRXFeedbackDevice.CTRE_MagEncoder_Relative;
+    //     public static FollowerType DEFAULT_MOTOR_FOLLOWER_TYPE = FollowerType.PercentOutput;
+    // }
 
     public static class RobotMeasurements {
         //TODO find robot physical characteristics
@@ -63,51 +63,51 @@ public final class Constants {
         public static double LIMELIGHT_ANGLE_RADIANS = -1;
     }
     
-    public static class Units {
-        //Base units
-        Unit METER = new BaseUnit(Dimension.Length, 1d);
-        Unit KILOMETER = new BaseUnit(Dimension.Length, METER.getScalar() * 1000d);
-        Unit FEET = new BaseUnit(Dimension.Length, METER.getScalar() * 3.280839895d);
+    // public static class Units {
+    //     //Base units
+    //     Unit METER = new BaseUnit(Dimension.Length, 1d);
+    //     Unit KILOMETER = new BaseUnit(Dimension.Length, METER.getScalar() * 1000d);
+    //     Unit FEET = new BaseUnit(Dimension.Length, METER.getScalar() * 3.280839895d);
 
-        Unit SECOND = new BaseUnit(Dimension.Time, 1d);
-        Unit MINUTE = new BaseUnit(Dimension.Time, SECOND.getScalar() * 60d);
-        Unit HOUR = new BaseUnit(Dimension.Time, MINUTE.getScalar() * 60d);
-        Unit MILLISECOND = new BaseUnit(Dimension.Time, SECOND.getScalar() / 1000d);
-        Unit ENCODER_TIME = new BaseUnit(Dimension.Time, MILLISECOND.getScalar() * 100d);
+    //     Unit SECOND = new BaseUnit(Dimension.Time, 1d);
+    //     Unit MINUTE = new BaseUnit(Dimension.Time, SECOND.getScalar() * 60d);
+    //     Unit HOUR = new BaseUnit(Dimension.Time, MINUTE.getScalar() * 60d);
+    //     Unit MILLISECOND = new BaseUnit(Dimension.Time, SECOND.getScalar() / 1000d);
+    //     Unit ENCODER_TIME = new BaseUnit(Dimension.Time, MILLISECOND.getScalar() * 100d);
         
-        Unit KILOGRAM = new BaseUnit(Dimension.Mass, 1d);
+    //     Unit KILOGRAM = new BaseUnit(Dimension.Mass, 1d);
 
-        Unit RADIAN = new BaseUnit(Dimension.Angle, 1d);
-        Unit ROTATION = new BaseUnit(Dimension.Angle, RADIAN.getScalar() * 2d * Math.PI);
-        Unit DEGREE = new BaseUnit(Dimension.Angle, ROTATION.getScalar() / 360d);
-        Unit ENCODER_ANGLE = new BaseUnit(Dimension.Angle, ROTATION.getScalar() / MotorConfig.TALON_ENCODER_RESOLUTION);
+    //     Unit RADIAN = new BaseUnit(Dimension.Angle, 1d);
+    //     Unit ROTATION = new BaseUnit(Dimension.Angle, RADIAN.getScalar() * 2d * Math.PI);
+    //     Unit DEGREE = new BaseUnit(Dimension.Angle, ROTATION.getScalar() / 360d);
+    //     Unit ENCODER_ANGLE = new BaseUnit(Dimension.Angle, ROTATION.getScalar() / MotorConfig.TALON_ENCODER_RESOLUTION);
 
-        Unit AMPERE = new BaseUnit(Dimension.Current, 1d);
+    //     Unit AMPERE = new BaseUnit(Dimension.Current, 1d);
 
-        //Compound units
-        Unit ENCODER_ANGULAR_VELOCITY = new CompoundUnit(ENCODER_ANGLE, ENCODER_TIME);
+    //     //Compound units
+    //     Unit ENCODER_ANGULAR_VELOCITY = new CompoundUnit(ENCODER_ANGLE, ENCODER_TIME);
 
-        Unit METERS_PER_SECOND = new CompoundUnit(METER, SECOND);
+    //     Unit METERS_PER_SECOND = new CompoundUnit(METER, SECOND);
 
-        Unit METERS_PER_SECOND_2 = new CompoundUnit(METERS_PER_SECOND, SECOND);
-        Unit NEWTON = new CompoundUnit(new Unit[] {KILOGRAM, METERS_PER_SECOND_2}, new Unit[] {});
-        Unit JOULE = new CompoundUnit(new Unit[] {NEWTON, METER}, new Unit[] {});
-        Unit COULOMB = new CompoundUnit(new Unit[] {AMPERE, SECOND}, new Unit[] {});
-        Unit VOLTAGE = new CompoundUnit(JOULE, COULOMB);
+    //     Unit METERS_PER_SECOND_2 = new CompoundUnit(METERS_PER_SECOND, SECOND);
+    //     Unit NEWTON = new CompoundUnit(new Unit[] {KILOGRAM, METERS_PER_SECOND_2}, new Unit[] {});
+    //     Unit JOULE = new CompoundUnit(new Unit[] {NEWTON, METER}, new Unit[] {});
+    //     Unit COULOMB = new CompoundUnit(new Unit[] {AMPERE, SECOND}, new Unit[] {});
+    //     Unit VOLTAGE = new CompoundUnit(JOULE, COULOMB);
 
         
-        //TODO How to implement scalar multipliers and angular->velocity?
+    //     //TODO How to implement scalar multipliers and angular->velocity?
         
-        //Old Code
-        //public static double ENCODER_VELOCITY_UNIT_TO_SECONDS = 0.1;//Encoder measures things in units per 0.1s
-        public static double DRIVETRAIN_ENCODER_DISTANCE_TO_METERS = 1 / MotorConfig.TALON_ENCODER_RESOLUTION * 2 * Math.PI * RobotMeasurements.DRIVETRAIN_WHEEL_RADIUS_METERS;
-        public static double DRIVETRAIN_ENCODER_VELOCITY_TO_METERS_PER_SECOND = DRIVETRAIN_ENCODER_DISTANCE_TO_METERS / ENCODER_VELOCITY_UNIT_TO_SECONDS;
-        public static double METERS_PER_SECOND_TO_DRIVETRAIN_ENCODER_VELOCITY = 1 / DRIVETRAIN_ENCODER_VELOCITY_TO_METERS_PER_SECOND;
-        public static double SHOOTER_ENCODER_VELOCITY_TO_METERS_PER_SECOND = 0;
-        public static double SPINNER_ENCODER_VELOCITY_TO_METERS_PER_SECOND = 0;
-        //public static double DRIVETRAIN_FEEDFORWARD_TO_ENCODER_UNITS = 1;//TODO find this number
-        //public static double SHOOTER_FEEDFORWARD_TO_ENCODER_UNITS = 1;//TODO find this number
-    }
+    //     //Old Code
+    //     //public static double ENCODER_VELOCITY_UNIT_TO_SECONDS = 0.1;//Encoder measures things in units per 0.1s
+        // public static double DRIVETRAIN_ENCODER_DISTANCE_TO_METERS = 1 / MotorConfig.TALON_ENCODER_RESOLUTION * 2 * Math.PI * RobotMeasurements.DRIVETRAIN_WHEEL_RADIUS_METERS;
+        // public static double DRIVETRAIN_ENCODER_VELOCITY_TO_METERS_PER_SECOND = DRIVETRAIN_ENCODER_DISTANCE_TO_METERS / ENCODER_VELOCITY_UNIT_TO_SECONDS;
+        // public static double METERS_PER_SECOND_TO_DRIVETRAIN_ENCODER_VELOCITY = 1 / DRIVETRAIN_ENCODER_VELOCITY_TO_METERS_PER_SECOND;
+        // public static double SHOOTER_ENCODER_VELOCITY_TO_METERS_PER_SECOND = 0;
+        // public static double SPINNER_ENCODER_VELOCITY_TO_METERS_PER_SECOND = 0;
+    //     //public static double DRIVETRAIN_FEEDFORWARD_TO_ENCODER_UNITS = 1;//TODO find this number
+    //     //public static double SHOOTER_FEEDFORWARD_TO_ENCODER_UNITS = 1;//TODO find this number
+    // }
 
     public static class SubsystemConfig {
         //Climb movement information
@@ -118,7 +118,7 @@ public final class Constants {
         //Drivetrain movement information
         public static double DRIVETRAIN_MAXIMUM_TESTED_ENCODER_VELOCITY = 3450;//Approx 4.03 meters per second
         public static double DRIVETRAIN_MAXIMUM_CRUISE_SPEED_METERS_PER_SECOND = 3.95;//Max is ~4
-        public static double DRIVETRAIN_MAXIMUM_MOVEMENT_SPEED_METERS_PER_SECOND = DRIVETRAIN_MAXIMUM_TESTED_ENCODER_VELOCITY * Conversions.DRIVETRAIN_ENCODER_VELOCITY_TO_METERS_PER_SECOND;
+        //public static double DRIVETRAIN_MAXIMUM_MOVEMENT_SPEED_METERS_PER_SECOND = DRIVETRAIN_MAXIMUM_TESTED_ENCODER_VELOCITY * Conversions.DRIVETRAIN_ENCODER_VELOCITY_TO_METERS_PER_SECOND;
         public static double DRIVETRAIN_CLOSED_LOOP_RAMP = 0.1; //seconds from 0 to full or full to 0
 
         //Shooter movement information
@@ -143,35 +143,35 @@ public final class Constants {
         };
     }
     
-    public static class MotionControl {
-        //PID
-        public static TalonSRXGains DRIVETRAIN_LEFT_PID = new TalonSRXGains(0.5, 0, 0);
-        public static TalonSRXGains DRIVETRAIN_RIGHT_PID = new TalonSRXGains(0.5, 0, 0);
-        public static TalonSRXGains SHOOTER_PID = new TalonSRXGains(0, 0, 0);
-        public static TalonSRXGains CLIMB_PID = new TalonSRXGains(0, 0, 0);
+    // public static class MotionControl {
+    //     //PID
+    //     public static TalonSRXGains DRIVETRAIN_LEFT_PID = new TalonSRXGains(0.5, 0, 0);
+    //     public static TalonSRXGains DRIVETRAIN_RIGHT_PID = new TalonSRXGains(0.5, 0, 0);
+    //     public static TalonSRXGains SHOOTER_PID = new TalonSRXGains(0, 0, 0);
+    //     public static TalonSRXGains CLIMB_PID = new TalonSRXGains(0, 0, 0);
 
-        //Feedforward
-        public static double DRIVETRAIN_FEEDFORWARD_KV_UNITS = 1 / 12 / MotorConfig.TALON_ENCODER_RESOLUTION * 10;
-        public static double DRIVETRAIN_FEEDFORWARD_KS_UNITS = 1 / 12;
-        public static SimpleMotorFeedforward DRIVETRAIN_FEEDFORWARD = new SimpleMotorFeedforward(0.843 * DRIVETRAIN_FEEDFORWARD_KS_UNITS, 0.362 * DRIVETRAIN_FEEDFORWARD_KV_UNITS, 0);
-        public static SimpleMotorFeedforward SHOOTER_FEEDFORWARD = new SimpleMotorFeedforward(0, 0, 0);
+    //     //Feedforward
+    //     public static double DRIVETRAIN_FEEDFORWARD_KV_UNITS = 1 / 12 / MotorConfig.TALON_ENCODER_RESOLUTION * 10;
+    //     public static double DRIVETRAIN_FEEDFORWARD_KS_UNITS = 1 / 12;
+    //     public static SimpleMotorFeedforward DRIVETRAIN_FEEDFORWARD = new SimpleMotorFeedforward(0.843 * DRIVETRAIN_FEEDFORWARD_KS_UNITS, 0.362 * DRIVETRAIN_FEEDFORWARD_KV_UNITS, 0);
+    //     public static SimpleMotorFeedforward SHOOTER_FEEDFORWARD = new SimpleMotorFeedforward(0, 0, 0);
 
-        //The following two could possibly just be normal PID values
-        public static TalonSRXGains LIMELIGHT_SHOOTER_PID = new TalonSRXGains(0, 0, 0);
-        public static TalonSRXGains LIMELIGHT_DRIVETRAIN_PID = new TalonSRXGains(0, 0, 0);
+    //     //The following two could possibly just be normal PID values
+    //     public static TalonSRXGains LIMELIGHT_SHOOTER_PID = new TalonSRXGains(0, 0, 0);
+    //     public static TalonSRXGains LIMELIGHT_DRIVETRAIN_PID = new TalonSRXGains(0, 0, 0);
 
-        public static double LIMELIGHT_SHOOTER_KP = 0.5;
-        public static double LIMELIGHT_SHOOTER_KI = 0;
-        public static double LIMELIGHT_SHOOTER_KD = 0;
+    //     public static double LIMELIGHT_SHOOTER_KP = 0.5;
+    //     public static double LIMELIGHT_SHOOTER_KI = 0;
+    //     public static double LIMELIGHT_SHOOTER_KD = 0;
 
-        public static double LIMELIGHT_DRIVETRAIN_KP = 0.5;
-        public static double LIMELIGHT_DRIVETRAIN_KI = 0;
-        public static double LIMELIGHT_DRIVETRAIN_KD = 0;
+    //     public static double LIMELIGHT_DRIVETRAIN_KP = 0.5;
+    //     public static double LIMELIGHT_DRIVETRAIN_KI = 0;
+    //     public static double LIMELIGHT_DRIVETRAIN_KD = 0;
 
-        public static double CLIMB_BALANCE_KP = 0.5;
-        public static double CLIMB_BALANCE_KI = 0;
-        public static double CLIMB_BALANCE_KD = 0;
-    }
+    //     public static double CLIMB_BALANCE_KP = 0.5;
+    //     public static double CLIMB_BALANCE_KI = 0;
+    //     public static double CLIMB_BALANCE_KD = 0;
+    // }
 
     //Miscellaneous
     public static double JOYSTICKF310_AXIS_DEADBAND = 0.05;
@@ -200,13 +200,13 @@ public final class Constants {
     public static double INSERTER_MOTOR_SPEED = 0.4;
 
     //Classes
-    public static class TalonSRXGains extends SlotConfiguration {
+    // public static class TalonSRXGains extends SlotConfiguration {
 
-        public TalonSRXGains(double kP, double kI, double kD) {
-            super();
-            this.kP = kP;
-            this.kI = kI;
-            this.kD = kD;
-        }
-    }
+    //     public TalonSRXGains(double kP, double kI, double kD) {
+    //         super();
+    //         this.kP = kP;
+    //         this.kI = kI;
+    //         this.kD = kD;
+    //     }
+    // }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,44 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+import frc.robot.JoystickF310.*;
+import frc.robot.DriveUtil.*;
+
+import static frc.robot.Constants.*;
 
 public class RobotContainer {
     // The robot's subsystems and commands are defined here...
+    
+    private final JoystickF310 joystickDrive = new JoystickF310(Ports.PORT_JOYSTICK_DRIVE);
+
+    // Create subsystems
+    private final DrivetrainSubsystem m_drivetrain = new DrivetrainSubsystem();
+
+    //Drivetrain
+    private final TeleopArcadeDriveCommand m_teleopArcadeDriveCommand = new TeleopArcadeDriveCommand(m_drivetrain);
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
-        // Configure the button bindings
-        configureButtonBindings();
+        // Configure all the control bindings
+        configureControlBindings();
     }
 
-    private void configureButtonBindings() {}
+    private void configureControlBindings() {
+        
+        //Configure control in the same order as the subsystems (alphabetical)
+
+        //Drivetrain
+        m_teleopArcadeDriveCommand.setSuppliers(
+            () -> DriveUtil.powCopySign(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), JOYSTICK_INPUT_EXPONENT),
+            () -> DriveUtil.powCopySign(joystickDrive.getRawAxis(AxisF310.JoystickRightX), JOYSTICK_INPUT_EXPONENT)
+            );
+
+        CommandScheduler.getInstance().setDefaultCommand(m_drivetrain, m_teleopArcadeDriveCommand);
+    }
 
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
