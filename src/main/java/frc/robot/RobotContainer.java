9401064eec5c23d9b89c693a1263c3c1ae1422d4
Dat-Boi-Arjun <56085610+Dat-Boi-Arjun@users.noreply.github.com>
2022-02-21 15:55:14 -0800

    Added switching functionality between manual and automatic turret modes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,9 +25,12 @@ public class RobotContainer {
     private final TurretSubsystem m_turret = new TurretSubsystem();
     //Create Commands
     private final TeleopArcadeDriveCommand m_teleopArcadeDriveCommand = new TeleopArcadeDriveCommand(m_drivetrain);
-    private final FlywheelPrototypeTestCommand m_flywheelPrototypeTestCommand = new FlywheelPrototypeTestCommand(m_drivetrain);
+    //private final FlywheelPrototypeTestCommand m_flywheelPrototypeTestCommand = new FlywheelPrototypeTestCommand(m_drivetrain);
     private final IntakeCommand m_intakeCommand = new IntakeCommand(m_intake);
+    private final LimelightTurretAimCommand m_limelightTurretAimCommand = new LimelightTurretAimCommand(m_turret);
     private final TurretManualCommand m_turretManualCommand = new TurretManualCommand(m_turret);
+    private final ZeroTurretCommand m_zeroTurretCommand = new ZeroTurretCommand(m_turret);
+    private final LimelightShooterCommand m_limelightShooterCommand = new LimelightShooterCommand(m_shooter);
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
@@ -55,10 +58,17 @@ public class RobotContainer {
             () -> DriveUtil.powCopySign(joystickOperator.getRawAxis(AxisF310.JoystickRightX), JOYSTICK_INPUT_EXPONENT)
         );
 
-        
+        // Configures the switching between manual and automatic turret modes
+        // It's only possible to zero the turret when its button is pressed and the mode is toggled to manual control
+        // The turret manual command is automatically scheduled when manual mode is toggled
+        // When it's toggled off, the default limelight turret aiming command will run
+        joystickOperator.getButton(ButtonF310.Y).toggleWhenActive(m_turretManualCommand)
+            .and(joystickOperator.getButton(ButtonF310.B)).toggleWhenActive(m_zeroTurretCommand);
         //
         CommandScheduler.getInstance().setDefaultCommand(m_drivetrain, m_teleopArcadeDriveCommand);
         CommandScheduler.getInstance().setDefaultCommand(m_intake, m_intakeCommand);
+        CommandScheduler.getInstance().setDefaultCommand(m_turret, m_limelightTurretAimCommand);
+        CommandScheduler.getInstance().setDefaultCommand(m_shooter, m_limelightShooterCommand);
     }
 
     public Command getAutonomousCommand() {
