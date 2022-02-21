4028cbdb35324decb75e57c6a975d0fb2e11fc8d
aklein123 <56097148+aklein123@users.noreply.github.com>
2022-02-21 12:12:47 -0800

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,10 +1,7 @@
 package frc.robot.subsystems;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.DriveUtil;
 import static frc.robot.Constants.*;
 
 public class IntakeSubsystem extends SubsystemBase {
